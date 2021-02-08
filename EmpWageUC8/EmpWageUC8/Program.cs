1198c6eb8a0dac9caaffcdfb3ea3539d32e1f6c1
sonal <sonalkarle01@gmail.com>
2021-02-08 06:16:19 +0530

    Welcome

---
+++ EmpWageUC8/EmpWageUC8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageUC8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome empWage computation");
+        }
+    }
+}
